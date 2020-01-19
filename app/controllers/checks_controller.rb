2bc5ccea2ad14f06fd24ff20efec9d321241c40e
Dima-A <Dima_A2010@mail.ru>
2020-01-20 02:20:41 +0300

    Small fix

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,10 +1,28 @@
+# class ChecksController < ApplicationController
+#   protect_from_forgery :except => [:method, :text]
+#   def index
+#     if params[:method] == "palindrome"
+#       @data = params[:text].palindrome?
+#     elsif params[:method] == "brackets"
+#       @data = params[:text].valid_brackets?
+#     end
+#   end
+# end
+
 class ChecksController < ApplicationController
   protect_from_forgery :except => [:method, :text]
   def index
-    if params[:method] == "palindrome"
-      @data = params[:text].palindrome?
-    elsif params[:method] == "brackets"
-      @data = params[:text].valid_brackets?
+    case params[:method]
+      when "palindrome"
+        params[:result] = params[:text].palindrome?
+      when "brackets"
+        params[:result] = params[:text].valid_brackets?
+    end
+    case params[:result]
+      when true
+        params[:result] = "да"
+      when false
+        params[:result] =  "нет"
     end
   end
 end
