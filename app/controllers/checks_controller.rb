79e21536ad89fc7604a712de8e30540473354398
Dima-A <Dima_A2010@mail.ru>
2020-02-01 14:42:21 +0300

    moved current_user check in controller

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,9 +1,13 @@
 class ChecksController < ApplicationController
-  protect_from_forgery :except => [:method, :text]
+  skip_before_action :verify_authenticity_token
+
   def index
+    check_user_unauthorized
     @data = Check.all
   end
+
   def create
+    check_user_unauthorized
     @data = Check.new(data_params)
     @data[:result] = case @data[:method]
                      when "palindrome"
@@ -17,5 +21,10 @@ class ChecksController < ApplicationController
   private
     def data_params
       params.permit(:method, :text, :token)
+    end
+    def check_user_unauthorized
+      if !current_user
+        render "shared/unauthorized"
+      end
     end
 end
